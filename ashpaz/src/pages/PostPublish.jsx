0e0d78307ba00acd693326d0639fca7c4937535a
Reza Karimi <rezakrimi@gmail.com>
2020-07-26 23:38:05 -0400

    creating myCKEditor

--- ashpaz/src/pages/PostPublish.jsx
+++ ashpaz/src/pages/PostPublish.jsx
@@ -1,38 +1,31 @@
 import React, { useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
-import CKEditor from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import '../styles/CustomStyles.css';
-import '../styles/CKEditor.css';
+import CKEditor from "../components/myCKEditor";
+import "../styles/CustomStyles.css";
+import "../styles/CKEditor.css";
 
-
-function PostPublish(){
-    const [content, setContent] = useState('');
+function PostPublish() {
+    const [content, setContent] = useState("");
 
     return (
-        <div className="App">
-            <div className="ck-content" dangerouslySetInnerHTML={{__html: content}}></div>
-            <h2>Using CKEditor 5 build in React</h2>
-            <CKEditor
-                editor={ ClassicEditor }
-                data="<p>Hello from Ozgol City</p>"
-                onInit={ editor => {
-                    // You can store the "editor" and use when it is needed.
-                    console.log( 'Editor is ready to use!', editor );
-                } }
-                onChange={ ( event, editor ) => {
-                    setContent(editor.getData());
-                } }
-                config={{
-                    alignment: {
-                        options: [ 'left', 'right' ]
-                    },
-                    toolbar: [
-                        'heading', '|', 'bulletedList', 'numberedList', 'alignment', 'undo', 'redo'
-                    ]
-                }}
-            />
-        </div>
+        <Container
+            fluid={true}
+            style={{ paddingLeft: "20px", paddingRight: "20px" }}
+        >
+            <Row>
+                <div
+                    className="ck-content"
+                    dangerouslySetInnerHTML={{ __html: content }}
+                ></div>
+            </Row>
+            <Row>
+                <Col xs={12}>
+                    <div className="App">
+                        <CKEditor handler={setContent} />
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
