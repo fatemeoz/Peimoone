7f563004088639ff34eca5a24bf405f609ce1fe1
fateme <fateme@fatemeozgoli@gmail.com>
2020-08-03 00:18:21 +0430

    Password Hash

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,16 +1,17 @@
 const router = require('express').Router();
 const User = require('../models/User')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+
+const saltRounds = 11;
 router.get('/', (req, res) => {
     res.send('users api');
 });
 
 router.post('/login', async (req, res) => {
-    
-    console.log(req.body)
     const user = await User.findOne({username: req.body.username});
     if(!user) return res.status(400).send('Username doesnt exists');
-    const validpass = await User.findOne({password: req.body.password});
+    const validpass = await bcrypt.compare(req.body.password, user.password);
     if (!validpass) return res.status(400).send("Invalid password");
     const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET)
     res.header('auth-token', token).json({
